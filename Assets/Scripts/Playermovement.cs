4eadd56a47e5bb34e072f9ef5760875aa7485466
Nathan Caley <ncaley2@gmail.com>
2026-01-15 16:02:05 -0500

    Episode 1 completed: Added FPS control movement, jump, and look

---
+++ Assets/Scripts/Playermovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Playermovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+           
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    void OnJump()
+    {
+        rb.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
+    }
+
+    void OnMovement(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    void MovePlayer()
+    {
+        Vector3 direction = (transform.right * moveInput.x) + (transform.forward * moveInput.y);
+        direction = direction.normalized;
+    
+    rb.linearVelocity = new Vector3(direction.x * moveSpeed, rb.linearVelocity.y, direction.z * moveSpeed);
+    }
+
+}
